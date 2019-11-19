e43e25f4c63799a45a3b26d3f755b6f309ae0f91
xaviguasch <xaviguasch@gmail.com>
2019-11-19 09:33:02 +0100

    splitted code into files

--- assets/scripts/Utility/Analytics.js
+++ assets/scripts/Utility/Analytics.js
@@ -1,8 +1,7 @@
-
 const intervalId = setInterval(() => {
   console.log('Sending analytics data...')
-}, 2000);
+}, 2000)
 
 document.getElementById('stop-analytics-btn').addEventListener('click', () => {
-  clearInterval(intervalId);
-});
+  clearInterval(intervalId)
+})
